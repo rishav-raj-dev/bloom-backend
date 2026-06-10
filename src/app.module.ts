2701e21a843ea1406db96a91bf0f9ce6b08a5dd5
rishav-raj-dev <social.rishav.2003@gmail.com>
2026-06-11 00:43:13 +0530

    wire up AuthMiddleware to all protected routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './modules/users/users.controller';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { AuthMiddleware } from './middlewares/auth.middleware';
 import { HabitModule } from './modules/habit/habit.module';
 import { DatabaseModule } from './modules/database/database.module';
 
@@ -18,5 +19,13 @@ export class AppModule implements NestModule {
     consumer
       .apply(LoggerMiddleware)
       .forRoutes('');
+
+    consumer
+      .apply(AuthMiddleware)
+      .exclude(
+        { path: 'auth/login', method: RequestMethod.POST },
+        { path: 'auth/register', method: RequestMethod.POST },
+      )
+      .forRoutes('*');
   }
 }
